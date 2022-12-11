a30403fe60c9f3ab1785047153e9c4441ea0468c
fahricigdem <fahricigdem34@gmail.com>
2022-12-11 21:32:59 +0100

    Orders

---
+++ ClassLibEntities/Concrete/Order.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibEntities.Concrete
+{
+    public class Order : IEntity
+    {
+        public int OrderID { get; set; }
+        public string CustomerId { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipCity { get; set; }
+
+
+    }
+}
---
+++ DataAccessClassLib/Abstract/IOrderDal.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibEntities.Concrete;
+using Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibDataAccess.Abstract
+{
+    public interface IOrderDal : IEntityRepository<Order>
+    {
+    }
+}
