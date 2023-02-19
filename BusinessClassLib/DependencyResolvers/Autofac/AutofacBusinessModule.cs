5fe4ef73b3b7fe6014c0e406552fffcd9914c771
fahri.cigdem <fahri.cigdem@atsonline.de>
2023-02-19 20:01:47 +0100

    - implement Autofac.Extensions.DependencyInjection

---
+++ BusinessClassLib/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using ClassLibBusiness.Abstract;
+using ClassLibBusiness.Concrete;
+using ClassLibDataAccess.Abstract;
+using ClassLibDataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibBusiness.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EFProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using ClassLibBusiness.Abstract;
 using ClassLibBusiness.Concrete;
+using ClassLibBusiness.DependencyResolvers.Autofac;
 using ClassLibDataAccess.Abstract;
 using ClassLibDataAccess.Concrete.EntityFramework;
 
@@ -8,9 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //Autofac, Ninject, CastleWindsor, StructoreMap, LightInject, DryInject --> IoC Container gibi islem yapan eski sistemler
+//AOP (Aspect Oriented Programming)
+//Postsharp
+// ProductManager de IProductDal'a bagimlidir, constructor'inda, dolayisiyla o noktada ihtiyac duyuldugundan collection'a IProductDal icin EFProductDal verilir. Eger InMemoryProductDal kullanilmasi gerekirse ne olacak ???
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EFProductDal>(); // ProductManager de IProductDal'a bagimlidir, constructor'inda, dolayisiyla o noktada ihtiyac duyuldugundan collection'a IProductDal icin EFProductDal verilir. Eger InMemoryProductDal kullanilmasi gerekirse ne olacak ???
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EFProductDal>();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EFProductDal>(); 
 
 var app = builder.Build();
 
